c1775426d1930d8afcd52738feb78eb17a678c98
Gustavo Stelle Cunha <gustavo.stelle@ambevtech.com.br>
2022-08-25 20:11:46 -0300

    listening user

--- app/src/main/java/hbsis/wms/ambev/spokenapp/MainActivity.kt
+++ app/src/main/java/hbsis/wms/ambev/spokenapp/MainActivity.kt
@@ -1,43 +1,57 @@
 package hbsis.wms.ambev.spokenapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import hbsis.wms.ambev.spokenapp.ui.theme.SpokenAPPTheme
 
 class MainActivity : ComponentActivity() {
+   private val registerResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if(it.resultCode == Activity.RESULT_OK){
+            Log.e("Spoken",
+                it.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).toString()
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SpokenAPPTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Button(onClick = {
+                        listenSpoke()
+                    }) {
+                        Text(text = "Falar")
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SpokenAPPTheme {
-        Greeting("Android")
+    private fun listenSpoke() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+        }
+        registerResult.launch(intent)
     }
+
+
 }
